f25ade6f3e1db3bf33f03d149ea5b6c7e84fc0d9
mohamed-ben-kaddour <mohamed.ben-kaddour.1@ens.etsmtl.ca>
2025-03-18 18:44:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ supabase: Client = create_client(url, key)
 def download_excel():
     try:
         # Call the stored procedure using RPC
-        response = (supabase.rpc("hello_world") .execute())        
+        response = supabase.functions.invoke("hello-world", invoke_options={'body':{}})        
         print("Current method response:")
         if response.data:
             for row in response.data:
